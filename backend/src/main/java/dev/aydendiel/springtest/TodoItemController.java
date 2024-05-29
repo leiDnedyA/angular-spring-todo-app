0b0b39f78c27c895ed44632116e5d98c594a4e49
leiDnedyA <aydendiel@gmail.com>
2024-05-29 06:26:50 -0400

    feat: Added POST, PUT, and DELETE endpoints

--- backend/src/main/java/dev/aydendiel/springtest/TodoItemController.java
+++ backend/src/main/java/dev/aydendiel/springtest/TodoItemController.java
@@ -1,15 +1,15 @@
 
 package dev.aydendiel.springtest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 public class TodoItemController {
@@ -31,6 +31,36 @@ public class TodoItemController {
 
     @GetMapping("/todo-items/{id}")
     public List<TodoItem> get(@PathVariable String id) {
-    return db.stream().filter(item -> item.getId().equals(id)).collect(Collectors.toList());
+        if (db.containsKey(id)) {
+            return List.of(db.get(id));
+        }
+        return List.of();
+    }
+
+    @PostMapping("/todo-items/")
+    public List<TodoItem> post(@RequestBody TodoItem todoItem) {
+        String newKey = String.valueOf(db.size() + 1);
+        db.put(newKey, todoItem);
+        return List.of(db.get(newKey));
+    }
+
+    @PutMapping("/todo-items/{id}")
+    public List<TodoItem> put(@RequestBody TodoItem todoItem, @PathVariable String id) {
+        if (db.containsKey(id)) {
+            TodoItem currItem = db.get(id);
+            currItem.setName(todoItem.getName());
+        } else {
+            db.put(id, todoItem);
+        }
+        return List.of(db.get(id));
+    }
+
+    @DeleteMapping("/todo-items/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        if (db.containsKey(id)) {
+            db.remove(id);
+            return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<Void>(HttpStatusCode.valueOf(404));
     }
 }
