72c85c08da667ee6595348c22b584d6935bda698
leiDnedyA <aydendiel@gmail.com>
2024-05-28 23:40:59 -0400

    feat: made very basic frontend components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, TodoItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-test';
+  todoItems = [{id: 0, title: 'Take out trash'}, {id: 1, title: 'Do laundry'}]
 }
---
+++ frontend/src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'todo-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.css'
+})
+export class TodoItemComponent {
+  @Input() title = '';
+}
